10d7da11948a2b74254199d823674bbd3bc2544c
Shiwei-Zhang <934069150@qq.com>
2016-11-12 10:59:16 +0800

    rote

--- 1.express/1.express.js
+++ 1.express/1.express.js
@@ -2,4 +2,15 @@ var express=require('express');
 
 var app=express();
 
-app.listen(9090);
+
+//当客户端通过GET的请求方式，访问/路径的时候
+app.get('/',function(req,res){//请求方式和请求路径
+    res.end('首页')
+});
+app.get('/login',function(req,res){
+    res.end('登录页')
+});
+app.get('/user',function(req,res){
+    res.end('用户主页')
+});
+app.listen(8080);
