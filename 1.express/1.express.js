3a4daa6c2325f5d277d9ccc8ce2a99287f8559b9
Shiwei-Zhang <934069150@qq.com>
2016-11-12 11:30:53 +0800

    new

--- 1.express/1.express.js
+++ 1.express/1.express.js
@@ -13,4 +13,36 @@ app.get('/login',function(req,res){
 app.get('/user',function(req,res){
     res.end('用户主页')
 });
+
+//定制404提示，当请求的路径没有路由能处理的时候，返回你请求的页面不存在
+//all匹配所有的方法
+//*匹配所有的方法
+app.all('*',function(req,res){
+    res.end('你请求的页面不存在')
+});
+
+
+
+
+//从上到下执行路由，匹配上执行此方法，匹配不上继续往下匹配
+//请求方式和路径一样的情况下，执行前面的方法
+
+//使用gitbash 发起post请求
+//curl -X POST http://localhost:9090/login
+
+//如果请求出错，配置环境变量  git bin
+
+
+//npm install nrm -g  切换源
+
+//npm ls
+//nrm test
+//nrm user <>
+
+/*
+ npm install nrm -g
+ nrm add zhufeng http://172.18.0.199
+ nrm use zhufeng
+
+*/
 app.listen(8080);
